469fc8843a2fefc8b6e3777a48b1764577a1f934
Gabe Tao <93845863+t-ebag@users.noreply.github.com>
2022-09-27 14:03:32 -0500

    script.py v2

--- script.py
+++ script.py
@@ -2,13 +2,24 @@
 # Gabe Tao
 import os
 commands = {
-  "enable ufw firewall": ["sudo ufw enable",
-                         "sudo ufw allow ssh/tcp",
-                         "sudo ufw logging on",
-                         "sudo ufw status"],
-  "search for media files": ["sudo find /home -iregex '.*\.\(mp3\|mp4\|m4a\|mov\|aac\|ogg\|webm\|flac\|jpg\|gif\|png\|jpeg\|tiff\)$'"],
-  "remove ftp": ["sudo apt-get remove -y ftp ftpd vsftpd pure-ftpd",
-                "sudo apt-get remove -y --purge ftp ftpd vsftpd pure-ftpd"]
+  "run Conduro security script": ["wget -O ./conduro.sh https://raw.githubusercontent.com/t-ebag/ubuntu/main/install.sh",
+                                  "chmod +x ./conduro.sh",
+                                  "./conduro.sh"],
+  "install password checking module": ["apt-get install libpam-pwquality"],
+  "install gufw": ["apt-get install gufw"],
+  "install bum": ["apt-get install bum"],
+  "install auditing program": ["apt-get install auditd"],
+  "enable audits": ["auditctl -e 1"],
+  "enable ufw firewall": ["ufw enable",
+                         "ufw allow ssh/tcp",
+                         "ufw logging on",
+                         "ufw status"],
+  "search for media files": ["/home -iregex '.*\.\(mp3\|mp4\|m4a\|mov\|aac\|ogg\|webm\|flac\|jpg\|gif\|png\|jpeg\|tiff\)$'"],
+  "remove ftp": ["apt-get autoremove -y --purge ftp ftpd vsftpd pure-ftpd"],
+  "remove samba": ["apt -y autoremove --purge samba samba-common smbclient"],
+  "list contents of rc.local": ["cat /etc/rc.local"],
+  "lock root user locally": ["passwd -l root"]
+  
 }
 
 for count, c in enumerate(commands):
@@ -18,7 +29,7 @@ for count, c in enumerate(commands):
   response = input("Proceed? (y/n)")
   if response == "y":
     print()
-    for i in commands[c]: os.system(i)
+    for i in commands[c]: os.system("sudo " + i)
     print("========== SUCCESS ==========")
   else:
 
