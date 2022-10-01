cb1442237e1bec619d84bf79cc774e54611b188c
Gabe Tao <93845863+t-ebag@users.noreply.github.com>
2022-10-01 12:07:25 -0500

    Update script.py

--- script.py
+++ script.py
@@ -9,24 +9,30 @@ commands = {
   "install gufw": ["apt-get install gufw"],
   "install bum": ["apt-get install bum"],
   "install auditing program": ["apt-get install auditd"],
+  "install antivirus": ["apt-get install clamtk"],
   "enable audits": ["auditctl -e 1"],
   "enable ufw firewall": ["ufw enable",
                          "ufw allow ssh/tcp",
                          "ufw logging on",
-                         "ufw status"],
+                         "ufw status verbose"],
   "search for media files": ["find /home -iregex '.*\.\(mp3\|mp4\|m4a\|mov\|aac\|ogg\|webm\|flac\|jpg\|gif\|png\|jpeg\|tiff\)$'"],
   "remove ftp": ["apt-get autoremove -y --purge ftp ftpd vsftpd pure-ftpd"],
-  "remove samba": ["apt-get -y autoremove --purge samba samba-common smbclient"],
-  "remove avahi": ["apt-get purge avahi-daemon"],
-  "list contents of rc.local": ["cat /etc/rc.local"],
-  "lock root user locally": ["passwd -l root"]
+  "remove samba": ["apt-get autoremove -y --purge samba samba-common smbclient"],
+  "remove avahi": ["apt-get autoremove -y --purge avahi-daemon"],
+  "list contents of rc.local": ["cat /etc/rc.local | less"],
+  "lock root user locally": ["passwd -l root"],
+  "display groups": ["cat /etc/group | less"],
+  "check for shellshock vulnerability": ["env 'VAR=() { :;}; echo Bash is vulnerable!' 'FUNCTION()=() { :;}; echo Bash is vulnerable!' bash -c 'echo Bash is safe if this is the only line displayed.'"],
+  "search top for bad processes": ["top",
+                                   "ps -aux",
+                                   "ps -aux | grep python"]  
   
 }
 
 for count, c in enumerate(commands):
   print(f"========== Task #{count+1} ==========")
   print(f"This task will {c} and will run the command(s):")
-  for i in commands[c]: print(i)
+  for i in commands[c]: print("sudo " + i)
   response = input("Proceed? (y/n)")
   if response == "y":
     print()
