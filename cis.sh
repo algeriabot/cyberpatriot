ccb64a1799613089c0bc7732498344afb0a2b3b5
Gabe Tao <93845863+t-ebag@users.noreply.github.com>
2022-11-26 10:50:48 -0600

    don't run as root!

--- cis.sh
+++ cis.sh
@@ -6,15 +6,15 @@ mkdir cis
 cd cis
 
 echo "Installing Ansible:"
-apt install software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 
 echo "Fetching Playbook into playbook.yml:"
 wget https://raw.githubusercontent.com/t-ebag/cyberpatriot/main/playbook.yml
 
 echo "Fetching Tasks into ubuntu2204_cis:"
-git clone https://github.com/t-ebag/ubuntu2204_cis
+sudo git clone https://github.com/t-ebag/ubuntu2204_cis
 
 echo "All done! Make sure to check over defaults/main.yml before running." 
 echo "Run with:"
