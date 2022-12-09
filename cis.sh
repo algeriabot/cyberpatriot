5ee5f432bb54f8f55441da298a2f79b06e542cce
Gabe Tao <93845863+t-ebag@users.noreply.github.com>
2022-12-08 21:04:03 -0600

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-"Making directory:"
-cd ~
-mkdir cis
-cd cis
-
 echo "Installing Ansible:"
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
 echo "Fetching Playbook into playbook.yml:"
-wget https://raw.githubusercontent.com/t-ebag/cyberpatriot/main/playbook.yml
+# wget https://raw.githubusercontent.com/t-ebag/cyberpatriot/main/playbook.yml
 
-echo "Fetching Tasks into ubuntu2204_cis:"
-sudo git clone https://github.com/t-ebag/ubuntu2204_cis
+echo "Fetching Tasks into ubuntu2004_cis:"
+sudo git clone https://github.com/t-ebag/ubuntu2004_cis
 
 echo "All done! Make sure to check over defaults/main.yml before running." 
 echo "Run with:"
