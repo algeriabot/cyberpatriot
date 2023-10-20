fa17cc47cc23e694ad1019139f1d1bca0e1377ce
Gabe Tao <tao.gabe@gmail.com>
2023-10-19 21:50:14 -0500

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -6,10 +6,10 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
 echo "Fetching Playbook into playbook.yml:"
-# wget https://raw.githubusercontent.com/t-ebag/cyberpatriot/main/playbook.yml
+# wget https://raw.githubusercontent.com/algeriabot/cyberpatriot/main/playbook.yml
 
 echo "Fetching Tasks into ubuntu2004_cis:"
-sudo git clone https://github.com/t-ebag/ubuntu2004_cis
+sudo git clone https://github.com/algeriabot/ubuntu2004_cis
 
 echo "All done! Make sure to check over defaults/main.yml before running." 
 echo "Run with:"
